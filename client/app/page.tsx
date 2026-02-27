c4efcd445c8ae66a5214967d0a6827f7b68f5542
Shadaab Karim <karimshadaab510@gmail.com>
2026-02-27 11:06:51 +0530

    chore: added nextjs client app

---
+++ client/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+      <h1 className="text-6xl">Estate-Mgmt Home Page</h1>
+    </div>
+  );
+}
